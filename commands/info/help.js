6260d473fc477d6c62244a65f10a3e3f4b462d0b
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-09-10 12:19:44 +0530

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,7 +11,7 @@ module.exports ={
     const embed = new MessageEmbed()
    .setThumbnail(client.user.displayAvatarURL())
    .setTitle('Commands list🧾🧾')
-   .setDescription('⚠️__Moderation commands__:[5] ``kick``,``ban``,``clear``,``removerole``,``addrole``  \n \n🎉 __Fun commands__:[3]\n ``memes``,``spam``,``8ball`` \n \n ℹ️ __Info commands__ :[2]\n  ``avatar``,``ping``\n \n ⚒️__Utility commands__ :[1]\n ``emoji``')  
+   .setDescription('⚠️__Moderation commands__:[5] ``kick``,``ban``,``clear``,``removerole``,``addrole``  \n \n🎉 __Fun commands__:[3]\n ``memes``,``spam``,``8ball`` \n \n ℹ️ __Info commands__ :[2]\n  ``avatar``,``ping``,``help``\n \n ⚒️__Utility commands__ :[1]\n ``emoji``')  
     .setColor("LUMINOUS_VIVID_PINK")
    
  .setFooter(`Requested by ${message.author.tag}`)
