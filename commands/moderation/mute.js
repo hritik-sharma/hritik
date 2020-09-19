626010cb25162683d41f9e811022d712d7c5f32d
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-09-19 22:02:53 +0530

    fbfb

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -4,49 +4,61 @@ module.exports ={
     name:"mute",
     category:"moderation",
 
- run:async(client,msg,message,args)=>{
+ run:async(client,message,args)=>{
 
 
-if (!msg.member.hasPermission('MANAGE_CHANNELS')) return msg.reply({embed:{color:'RED',description:'you do not have the permissions to use this command'}})
+if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply({embed:{color:'RED',description:'you do not have the permissions to use this command'}})
 
-var user = msg.mentions.users.first();
+var user = message.mentions.users.first();
 
-if (!user) return msg.reply({embed:{color:'RED',description:'mention a user to mute '}})
+if (!user) return message.reply({embed:{color:'RED',description:'mention a user to mute '}})
 
 var member;
  try {
-     member = await msg.guild.members.fetch(user);
+     member = await message.guild.members.fetch(user);
  } catch(err) {
 
     member = null;
  }
 
-if (!member) return msg.reply({embed:{color:'RED',description:'the mentioned user is not in the server'}})
+if (!member) return message.reply({embed:{color:'RED',description:'the mentioned user is not in the server'}})
 
 if (member) {
- if (member.hasPermission('MANAGE_CHANNELS')) return msg.reply({embed:{color:'RED',description:'cannot mute the user has manage channels permission'}})
+ if (member.hasPermission('MANAGE_CHANNELS')) return message.reply({embed:{color:'RED',description:'cannot mute the user has manage channels permission'}})
 
 
-let muterole = message.guild.roles.cache.find(r = r.name === "MUTED")
 
-var embed = new MessageEmbed
+ let muterole = message.guild.roles.cache.find( r => r.name === "muted");
+
+if (!muterole)  return message.reply({embed:{color:'RED',description:'cannot find a mute role pls make one!'}})
+
+if(member.roles.cache.has(muterole.id)) {
+    return message.channel.send("Given user is already muted")
+}
+
+var reason = args.slice(1).join(' ');
+if(!reason) return message.reply({embed:{color:"RED",description:"Mention a reason to mute the user"}})
+var embed = new MessageEmbed()
 
 .setTitle('user muted')
 .addField('User:',user,true)
-.addField('By:',msg.author, true)
+.addField('By:',message.author, true)
+.addField('Reason:',reason)
 .setFooter(`Requested by ${message.author.tag}`)
 .setTimestamp()
+.setColor('GREEN')
 message.channel.send(embed)
 
 var embed = new MessageEmbed()
 .setTitle("You were muted!")
+.addField('Reason:',reason)
  try{
-      await user.send(embed)
+      await member.send(embed)
       }catch(err) {
            console.warn(err)
           }
-           msg.guild.members.muterole(user);
-            msg.channel.send({embed:{color:"GREEN",description:`${user} has been muted by ${msg.author}`}});
+           member.roles.add(muterole.id)
+            message.channel.send({embed:{color:"GREEN",description:`${user} has been muted by ${message.author}`}});
  }
         }
 }
