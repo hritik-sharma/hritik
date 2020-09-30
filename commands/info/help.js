e7c1c2c0fb556f737a09891e2e014f18fa564b7c
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-09-30 13:19:42 +0530

    f

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,7 +11,7 @@ module.exports ={
     const embed = new MessageEmbed()
    .setThumbnail(client.user.displayAvatarURL())
    .setTitle('Commands list🧾🧾')
-   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[4] \n  **memes**,**spam**,**8ball**,**rps**\n \n  ℹ️ __**Info Commands**__ :[7] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**Userid**,**servername**,**members(server)**\n \n  ⚒️__**Utility Commands**__ :[1] \n **emoji** \n \n __**Economy Commands**__ : **bal**')  
+   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[4] \n  **memes**,**spam**,**8ball**,**rps**\n \n  ℹ️ __**Info Commands**__ :[7] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**Userid**,**servername**,**members(server)**\n \n  ⚒️__**Utility Commands**__ :[1] \n **emoji** \n \n __**Economy Commands**__ : [1] \n **bal** \n \n COMING SOON!!!' )  
     .setColor("LUMINOUS_VIVID_PINK")
    
  .setFooter(`Requested by ${message.author.tag}`)
