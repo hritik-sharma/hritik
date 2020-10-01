e926a40b2e94ada71eff7beeb1f7dce8c215ea4d
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-01 13:42:50 +0530

    dgdg

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,7 +11,6 @@ module.exports ={
     const embed = new MessageEmbed()
    .setThumbnail(client.user.displayAvatarURL())
    .setTitle('Commands list🧾🧾')
-   .addField({name:Prefix , value:'/' })
    .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[4] \n  **memes**,**spam**,**8ball**,**rps**\n \n  ℹ️ __**Info Commands**__ :[7] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**Userid**,**servername**,**members(server)**\n \n  ⚒️__**Utility Commands**__ :[1] \n **emoji** \n \n __**Economy Commands**__ : [2] \n **bal** , **beg**\n COMING SOON!!!' )  
     .setColor("LUMINOUS_VIVID_PINK")
     .addField("**Fresh Force link**"," [Click Here](https://discord.com/oauth2/authorize?client_id=750379372485279815&scope=bot&permissions=8) ")
