359e6b182b246e054a8885d83bb6d0f8c157a966
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-01 13:26:31 +0530

    jbjb

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,9 +11,10 @@ module.exports ={
     const embed = new MessageEmbed()
    .setThumbnail(client.user.displayAvatarURL())
    .setTitle('Commands list🧾🧾')
-   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[4] \n  **memes**,**spam**,**8ball**,**rps**\n \n  ℹ️ __**Info Commands**__ :[7] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**Userid**,**servername**,**members(server)**\n \n  ⚒️__**Utility Commands**__ :[1] \n **emoji** \n \n __**Economy Commands**__ : [1] \n **bal** \n \n COMING SOON!!!' )  
+   .addField("Prefix - /")
+   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[4] \n  **memes**,**spam**,**8ball**,**rps**\n \n  ℹ️ __**Info Commands**__ :[7] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**Userid**,**servername**,**members(server)**\n \n  ⚒️__**Utility Commands**__ :[1] \n **emoji** \n \n __**Economy Commands**__ : [1] \n **bal** , **beg**\n \n COMING SOON!!!' )  
     .setColor("LUMINOUS_VIVID_PINK")
-   
+    .addField("**Fresh Force link**"," [Click Here](https://discord.com/oauth2/authorize?client_id=750379372485279815&scope=bot&permissions=8) ")
  .setFooter(`Requested by ${message.author.tag}`)
    .setTimestamp()
 
