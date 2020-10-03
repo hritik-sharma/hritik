ccdc81f1abf919bb8aa1297e5a31d2ab542a2109
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-03 15:21:19 +0530

    ffgn

---
+++ commands/utility/announce.js
@@ -0,0 +1,29 @@
+const{MessageEmbed,Message} = require ("discord.js")
+
+module.exports ={
+    name:"announce",
+    category:"moderation",
+
+    run:async(client,msg,args) =>{
+     let mesg = args.join(" ");
+     if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.reply({embed:{color:'RED',description:'you do not have the permissions to use this command'}})
+     if(!mesg) return msg.reply({embed:{color:"RED",description:"please write something to announce \n Command usage: ```/announce  (channel_name) (write_announcement)``` \n Remember: brackets are just for an instance"}})
+    
+
+     let everyone = msg.guild.roles.cache.find( r => r.name === "everyone");
+    
+let channel1 = msg.mentions.channels.first()
+
+if(!channel1) return msg.reply({embed:{color:"RED",description:"please mention a channel to announce \n Command usage: ```/announce (channel_name)  (write_announcement)```  \n Remember: brackets are just for an instance"}})
+
+     var embed = new MessageEmbed()
+ .setTitle('New Server Announcement')
+.addField("Alert",mesg,true)
+.setColor("PINK")
+.setFooter(`Announcement by ${msg.author.tag}`)
+     channel1.send(embed)
+
+     msg.channel.send({embed:{color:"GREEN",description:`**Done!** \n Announcement has been sent to ${channel1} `}});
+
+    }
+}
---
+++ commands/utility/dm.js
@@ -0,0 +1,22 @@
+const{MessageEmbed,Message} = require ("discord.js")
+
+module.exports ={
+    name:"dmm",
+    category:"moderation",
+
+    run:async(client,msg,args) =>{
+     let mesg = args.join(" ");
+
+     var user = msg.mentions.users.first();
+          
+     let guild = msg.guild.name
+    
+     var embed = new MessageEmbed()
+.addField('Alert from server:', guild ,true)
+.addField('Alert:',mesg, true)
+.setColor('random')
+.setFooter(`sender ${msg.author.tag}`)
+     user.send(embed) 
+
+            }
+        }
