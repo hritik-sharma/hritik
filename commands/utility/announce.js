0ad51b5a39e61f48c0533dffd1e39f4ad863774b
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-03 17:54:26 +0530

    fgngfn

--- commands/utility/announce.js
+++ commands/utility/announce.js
@@ -2,19 +2,14 @@ const{MessageEmbed,Message} = require ("discord.js")
 
 module.exports ={
     name:"announce",
-    category:"moderation",
+    category:"utility",
 
     run:async(client,msg,args) =>{
-    
-    
-if (!msg.member.hasPermission("MANAGE_CHANNELS"))  return msg.reply({embed:{color:'RED',description:'you do not have the permission to use this command'}})
-
      let mesg = args.slice(1).join(" ");
-
+     if (!msg.member.hasPermission('MANAGE_CHANNELS')) return msg.reply({embed:{color:'RED',description:'you do not have the permissions to use this command'}})
      if(!mesg) return msg.reply({embed:{color:"RED",description:"please write something to announce \n Command usage: ```/announce  (channel_name) (write_announcement)``` \n Remember: brackets are just for an instance"}})
+    
 
-
-     let everyone = msg.guild.roles.cache.find( r => r.name === "everyone");
     
 let channel1 = msg.mentions.channels.first()
 
