b59a8dec21c6e9e12181de30b04e62691e745bcc
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-06 20:39:26 +0530

    vcv

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,7 +11,7 @@ module.exports ={
     const embed = new MessageEmbed()
    .setThumbnail(client.user.displayAvatarURL())
    .setTitle('Commands list🧾🧾')
-   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[5] \n  **memes**,**spam**,**8ball**,**rps**,**trivia**\n \n  ℹ️ __**Info Commands**__ :[9] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**userid**,**servername**,**members(server)**,**servericon**\n \n  ⚒️__**Utility Commands**__ :[8] \n **emoji**,**dm**,**announce**,**enlarge(emoji)**,**createchannel**,**poll**,**giveaway**,**say** \n \n __**Economy Commands**__ : [2] \n **bal** , **beg**\n COMING SOON!!!' )  
+   .setDescription('⚠️__**Moderation Commands**__:[9] \n**kick**,**ban**,**clear**,**removerole**,**addrole**,**lock**,**unlock**,**mute**,**unmute** \n\n 🎉 __**Fun commands**__:[5] \n  **memes**,**spam**,**8ball**,**rps**,**trivia**,**joke**\n \n  ℹ️ __**Info Commands**__ :[9] **avatar**,**ping**,**help**,**botinfo**,**userinfo**,**userid**,**servername**,**members(server)**,**servericon**\n \n  ⚒️__**Utility Commands**__ :[8] \n **emoji**,**dm**,**announce**,**enlarge(emoji)**,**createchannel**,**poll**,**giveaway**,**say** \n \n __**Economy Commands**__ : [2] \n **bal** , **beg**\n COMING SOON!!!' )  
     .setColor("LUMINOUS_VIVID_PINK")
     .addField("**Fresh Force link**"," [Click Here](https://discord.com/oauth2/authorize?client_id=750379372485279815&scope=bot&permissions=8) ")
  .setFooter(`Requested by ${message.author.tag}`)
