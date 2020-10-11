64c3e5b566f7d7331de14c91c4585a81b170bc4d
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-11 16:19:46 +0530

    bb

--- commands/utility/dm.js
+++ commands/utility/dm.js
@@ -5,16 +5,25 @@ module.exports ={
     category:"moderation",
 
     run:async(client,msg,args) =>{
+
+        if(!msg.member.hasPermission('MANAGE_CHANNELS')) return msg.reply({embed:{color:"RED",description: 'You dont have permission to use this command'}});
+  
+        var user = msg.mentions.users.first();
+          
+        if (!user) return msg.reply ({embed:{color :"RED", description:"Mention a user to dm  \n usage: prefix dm @freshforce hello there"}});
+        
      let mesg = args.slice(1).join(" ");
 
-     var user = msg.mentions.users.first();
-          
+     if(!mesg) return msg.reply({embed:{color:"RED",description:"please write something in dm  \n usage: prefix dm @freshforce hello there"}})
+
+   
+
      let guild = msg.guild.name
     
      var embed = new MessageEmbed()
 .addField('Alert from server:', guild ,true)
 .addField('Alert:',mesg, true)
-.setColor('random')
+.setColor("GREEN")
 .setFooter(`sender ${msg.author.tag}`)
      user.send(embed) 
 
