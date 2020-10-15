c429dcfb22318bec78c67ada8ba75d6350c00bf5
hritik-sharma <70906210+hritik-sharma@users.noreply.github.com>
2020-10-15 21:33:28 +0530

    fgv

--- commands/moderation/addrole.js
+++ commands/moderation/addrole.js
@@ -13,11 +13,11 @@ module.exports ={
 
         let user = message.mentions.members.first()
         if(!user) return message.channel.send({embed:{color:"RED",description:"Mention a user for that role."}})
-        .then(m=> m.delete({timeout:10000}));
+        .then(m=> m.delete({timeout:5000}));
 
         let role = message.mentions.roles.first()
         if(!role) return message.channel.send({embed:{color:"RED",description:"Mention a role for that user."}})
-        .then(m=> m.delete({timeout:10000}));
+        .then(m=> m.delete({timeout:5000}));
 
         message.channel.send({embed:{color:"GREEN",description:`${role} has been given to ${user}`}})
         user.roles.add(role)
